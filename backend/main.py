129eafc5ee8c9e594b474cfaf9e5fe6fc884653e
anika-tahsin <anikatahsintonu@gmail.com>
2024-05-02 23:15:38 +0600

    create contacts

--- backend/main.py
+++ backend/main.py
@@ -10,11 +10,28 @@ from models import Contact
 @app.route("/contacts", methods=["GET"])
 def get_contacts():
     contacts = Contact.query.all()
-    json_contacts = list(map(lambda x:x.to_json(), contacts))
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
     return jsonify({"contacts":json_contacts})
 
 # read/get: first_name, last_name, email, phone
   # Request: 
+@app.route("/create_contacts", methods=["POST"])
+def create_contacts():
+    first_name = request.json.get("firstname")
+    last_name = request.json.get("lastname")
+    email = request.json.get("email")
+    phone = request.json.get("phone")
+
+    if not first_name or not last_name or not email or not phone:
+        return (
+            jsonify({"message": "You must include first name, last name, email and phone number"}), 
+        400,
+        )
+    
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email, phone=phone)
+           
+    
+    
 
 # update : any
     # Request: PUT, PATCH
