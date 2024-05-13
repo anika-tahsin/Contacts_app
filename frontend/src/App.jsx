9bd5bd26ced7be41fd9f1ea0b04d5aa9a1da7f3f
anika-tahsin <anikatahsintonu@gmail.com>
2024-05-13 21:59:26 +0600

    ContactForm uploading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,26 @@
-import { useState, useEffect } from 'react'
-import ContactList  from './ContactList'
-import './App.css'
+import { useState, useEffect } from 'react';
+import ContactList  from './ContactList';
+import './App.css';
+import ContactForm from './ContactForm';
 
 function App() {
   const [contacts, setContacts] = useState([])
 
   useEffect (() => {
     fetchConatacts()
-  }, [])
+  }, []);
 
 
   const fetchConatacts = async () => {
-     const response = await fetch("http://127.1.0.0:5000/contacts")
-     const data = await response.json()
-     setContacts(data.contacts)
-     console.log(data.contacts)
+     const response = await fetch("http://127.1.0.0:5000/contacts");
+     const data = await response.json();
+     setContacts(data.contacts);
+     console.log(data.contacts);
   }
 
 
-  return < ContactList contacts={contacts} />
+  return <>< ContactList contacts={contacts} />
+  <ContactForm /></>
 }
 
 export default App
--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -1,10 +1,87 @@
 import { useState } from "react";
 
-const ContactForm = ({}) => {
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [email, setEmail] = useState("")
-    const [phone, setPhone] = useState("")
+const ContactForm = ({ }) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
 
-    
-}
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        const data = {
+            firstName,
+            lastName,
+            email,
+            phone
+        }
+
+        const url = "http://127.0.0.1:5000/create_contact"
+
+        const options = {
+            method: "POST", 
+            header: {
+                "Content-Type": "application/json"
+            },
+            body : JSON.stringify(data)
+        }
+
+        const response = await fetch(url, options)
+        if (response.status !== 201 && response.status !== 200){
+            const data = await response.json()
+            alert (data.message)
+        } else {
+            //successful
+
+        }
+
+    }
+
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <label htmlFor="firstName"> First Name: </label>
+                <input 
+                    type="text" 
+                    id ="firstName" 
+                    value={firstName} 
+                    onChange={(e) => setFirstName(e.target.value )}
+
+                />
+            </div>
+
+            <div>
+                <label htmlFor="lastName"> Last Name: </label>
+                <input 
+                    type="text" 
+                    id ="lastName" 
+                    value={lastName} 
+                    onChange={(e) => setLastName(e.target.value )}
+                />
+            </div>
+
+            <div>
+                <label htmlFor="email"> Email: </label>
+                <input 
+                    type="text" 
+                    id ="email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value )}
+                />
+            </div>
+            <div>
+                <label htmlFor="phone"> Phone: </label>
+                <input 
+                    type="integer" 
+                    id ="phone" 
+                    value={phone} 
+                    onChange={(e) => setPhone(e.target.value )}
+                />
+            </div>
+            <button type="submit"> Create Contact </button>
+    </form>
+   );
+};
+
+export default ContactForm
